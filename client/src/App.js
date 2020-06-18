59917d4fb0194cb9c11f8dacddd4df96ae28523e
Weronika Stankiewicz <weronika.stankiewicz94@gmail.com>
2020-06-18 21:02:55 +0200

    add react-leaflet

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import { Icon } from "leaflet";
+import "./styles/index.css";
 
-function App() {
+export const App = () => {
+  const [state, setState] = useState({
+    lat: 51.505,
+    lng: -0.09,
+  });
+  const [zoom, setZoom] = useState(13);
+
+  // const [lat] = useState(51.505);
+  // const [lng] = useState(-0.09);
+  // const [zoom, setZoom] = useState(3);
+
+  let position = [state.lat, state.lng];
+  let defaultZoom = [zoom];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Map center={position} zoom={defaultZoom}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw"
+          id="mapbox/streets-v11"
+        />
+        <Marker position={position}>
+          <Popup>sample</Popup>
+        </Marker>
+      </Map>
+    </>
   );
-}
-
+};
 export default App;
